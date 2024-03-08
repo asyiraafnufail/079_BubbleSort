3b5bb07dc2a2d6eaa00db65582dbd9af6af7c0e3
asyiraafnufail <asyiraafnufail@gmail.com>
2024-03-08 15:49:34 +0700

    menambahkan bubble sort array

--- 079_BubbleSort/079_BubbleSort.cpp
+++ 079_BubbleSort/079_BubbleSort.cpp
@@ -20,3 +20,31 @@ void input() { // procedur untuk input
 	cout << "=====================" << endl;
 	cout << "masukkan elemen array" << endl;
 	cout << "=====================" << endl;
+
+	for (int i = 0; i < n; i++) { // looping dengan 1 dimulai dari 0 hingga n-1
+		cout << "data ke-" << (i + i) << "; "; // output ke layar
+		cin >> arr[i]; // output dari pengguna
+	}
+}
+
+void bubbleSortArray() { // procedur untuk mengurutkan array dengan metode bubble sort
+	int pass = 1; // step 1
+
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) { // step 2
+			if (arr[j] > arr[j + 1]) { // step 2
+				int temp;
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+		pass = pass + 1; // step 4
+		cout << "\nPass" << pass - 1 << "; "; // number of pass
+		for (int k = 0; k < n; k++) {
+			cout << arr[k] << " "; // menampilkan data pada number of pass
+		
+		}
+		cout << endl;
+	} while (pass <= n - 1); // step 5
+}
